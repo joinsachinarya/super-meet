455571f2588f04a3cdc2a32f18afa0d464024b4b
joinsachinarya <sachinkarya333@gmail.com>
2025-02-23 23:24:47 +0530

    add new template for the projec

--- app/utils/get-read-time.ts
+++
@@ -1,9 +0,0 @@
-export default function getReadTime(content: any, wordsPerMinute = 200) {
-  // Calculate the number of words in the content
-  const wordCount = content.split(/\s+/).length;
-
-  // Calculate the estimated read time in minutes
-  const readTimeMinutes = Math.ceil(wordCount / wordsPerMinute);
-
-  return readTimeMinutes;
-}
