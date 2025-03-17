d41182b93d630bc9c839f437518aa72b3382651d
joinsachinarya <sachinkarya333@gmail.com>
2025-03-17 10:24:14 +0530

    using socket server

--- app/context/socket-context.tsx
+++ app/context/socket-context.tsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import io from "socket.io-client";
 
 
-const URL = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_SOCKET_URL : "http://localhost:5000";
+const URL = process.env.NEXT_PUBLIC_DEPLOYMENT_ENV === "production" ? process.env.NEXT_PUBLIC_SOCKET_URL : "http://localhost:5000";
 const SocketContext = createContext(null);
 
 export const useSocket = () => {
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NotFount = () => {
     return (
-        <div className="grid min-h-screen place-items-center p-8 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid min-h-screen place-items-center p-8 ">
             <div className="flex flex-col items-center gap-6 text-center">
                 <h1 className="text-6xl font-bold">404</h1>
                 <h2 className="text-2xl font-medium">Page Not Found</h2>
